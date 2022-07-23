3d8971773c022f1a7a52274bf1dbeb16e3f12397
Vladislav Firsov <Valacardin@gmail.com>
2022-07-23 17:15:07 +0300

    My ceasar_cipher is ready

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -16,3 +16,50 @@ def take_key():
             key = input('key must be an integer between 1 and 26: ')
     return int(key)
 
+def cipher_sentence(sentence, key):
+    ciphered_sentence = ""
+    for i in sentence:
+        if i.isupper():
+            i = i.lower()
+            ciphered_sentence += eng_alphabet[(eng_alphabet.index(i) + key) %
+                                          len(eng_alphabet)].upper()
+        elif i in eng_alphabet:
+            ciphered_sentence += eng_alphabet[(eng_alphabet.index(i) + key) %
+                                          len(eng_alphabet)]
+        else:
+            ciphered_sentence += i
+    return ciphered_sentence
+
+
+def decipher_sentence(sentence, key):
+    ciphered_sentence = ""
+    for i in sentence:
+        if i.isupper():
+            i = i.lower()
+            ciphered_sentence += eng_alphabet[(eng_alphabet.index(i) - key) %
+                                              len(eng_alphabet)].upper()
+        elif i in eng_alphabet:
+            ciphered_sentence += eng_alphabet[(eng_alphabet.index(i) - key) %
+                                              len(eng_alphabet)]
+        else:
+            ciphered_sentence += i
+    return ciphered_sentence
+
+
+def main():
+    what_to_do = input('Please, enter a command: cipher or decipher?: ')
+    while what_to_do not in ('cipher, decipher'):
+        what_to_do = input('I don\'t understand, please, ender a command: cipher or decipher?: ')
+    sentence = take_sentence()
+    key = take_key()
+    if what_to_do == 'cipher':
+        print(cipher_sentence(sentence, key))
+    else:
+        print(decipher_sentence(sentence, key))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
